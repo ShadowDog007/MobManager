41220e6ba84099e551f0d1a9e33b8efe4013dc9a
Michael McKnight <michael@mcknight.ninja>
2015-03-08 23:50:12 +1300

    Start of rewrite

---
+++ src/main/java/ninja/mcknight/bukkit/mobmanager/plugin/MobManager.java
@@ -0,0 +1,11 @@
+package ninja.mcknight.bukkit.mobmanager.plugin;
+
+import ninja.mcknight.bukkit.bknightcore.plugin.BKPlugin;
+
+public class MobManager extends BKPlugin {
+
+	public MobManager() {
+		
+	}
+
+}
